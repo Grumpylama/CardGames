a02da7a6084f244728cd596a81dc3564e9e0c045
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-03-31 18:12:18 +0200

    Work on cardprinting (not done)

--- CardGames/Card.cs
+++ CardGames/Card.cs
@@ -18,11 +18,58 @@ namespace CardGames
             this.Suit = suit;
         }
         
-        public void DisplayCard(int x, int y, int size)
+        string getSuitString()
+        {
+            switch(Suit)
+            {
+                case 0:
+                    return "♣";
+                case 1:
+                    return "♦";
+                case 2:
+                    return "♥";
+                case 3:
+                    return "♠";
+                default:
+                    return " ";
+            }
+        }
+        
+        public void DisplayCard(int x, int y, int width, int height)
         {
 
             
             Console.SetCursorPosition(x, y);
+            for (int i = 0; i < height; i++)
+            {
+                
+                if (i == 0)
+                {
+                    Console.WriteLine("┌─────────┐");
+                }
+                else if (i == height - 1)
+                {
+                    Console.WriteLine("└─────────┘");
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("│ {0}       │", Value);
+                }
+                else if (i == height - 2)
+                {
+                    Console.WriteLine("│       {0} │", Value);
+                }
+                else if (i == height / 2)
+                {
+                    Console.WriteLine("│    {0}    │", Value);
+                }
+                else
+                {
+                    Console.WriteLine("│         │");
+                }
+                Console.SetCursorPosition(x, y + 1);
+
+            }
 
 
         }
--- CardGames/Program.cs
+++ CardGames/Program.cs
@@ -5,8 +5,19 @@ global using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
 
+namespace CardGames{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Card card = new Card(4, 4);
+            card.DisplayCard(1, 1, 123, 6);
+            Console.ReadLine();
+        }
+    }
+}
 
 
 
